757f56972ae671d98de8ce8ce6137ccf2650e215
aronhammond <hammond756@live.nl>
2015-04-02 13:23:48 +0200

    test

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Aron Hammond on 01/04/15.
+//  Copyright (c) 2015 mprog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTypingNumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        
+        if userIsTypingNumber
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit
+            userIsTypingNumber = true
+        }
+    }
+}
+
