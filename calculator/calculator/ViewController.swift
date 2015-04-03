2f4fc5383ddcdf627ffc4b58107befbb9307f699
aronhammond <hammond756@live.nl>
2015-04-03 21:58:38 +0200

    added comments

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -2,29 +2,33 @@
 //  ViewController.swift
 //  calculator
 //
+//  10437215
+//
 //  Created by Aron Hammond on 02/04/15.
 //  Copyright (c) 2015 mprog. All rights reserved.
 //
+// bug: in landscape mode verdwijnt het dispaly label. Heb dit niet kunnen verhelpen
 
 import UIKit
 import Darwin
 
 class ViewController: UIViewController
 {
-    
+    // connect outlets
     @IBOutlet weak var display: UILabel!
-    
     @IBOutlet weak var log: UILabel!
     
+    // declare variables for checking states and storing numbers
     var userIsTyping = false
     var isDoingOperation = false
     var numberTyped = ""
     
+    // link to number buttons
     @IBAction func appendDigit(sender: UIButton)
     {
         let digit = sender.currentTitle!
         
-        
+        // print number on screen and save to numberTyped for logging
         if userIsTyping
         {
             display.text = display.text! + digit
@@ -38,15 +42,18 @@ class ViewController: UIViewController
         }
     }
     
+    // link to the operators
     @IBAction func operate(sender: UIButton)
     {
         let operation = sender.currentTitle!
         
+        // shortcut for user experience
         if userIsTyping
         {
             enter()
         }
         
+        // preform required operation on operandStack
         switch operation
         {
         case "×": preformOperation {$0 * $1} 
@@ -54,6 +61,7 @@ class ViewController: UIViewController
         case "+": preformOperation {$0 + $1}
         case "−": preformOperation {$1 - $0}
         case "√": preformOperation { sqrt($0) }
+        // exception: pi is added to stack and shown in display
         case "π":
             operandStack.append(M_PI)
             display.text = "\(M_PI)"
@@ -62,9 +70,11 @@ class ViewController: UIViewController
         default: break
         }
         
+        // add the operator to the log
         log.text = log.text! + operation + ", "
     }
     
+    // compute the result for two operands and store in displayValue
     func preformOperation(operation: (Double, Double) -> Double)
     {
         if operandStack.count >= 2
@@ -74,6 +84,7 @@ class ViewController: UIViewController
         }
     }
     
+    // compute the result for one operands and store in displayValue
     func preformOperation(operation: Double -> Double)
     {
         if operandStack.count >= 2
@@ -83,10 +94,13 @@ class ViewController: UIViewController
         }
     }
     
+    // boolean for checking amount of points typed
     var numberIsFloat = false
     
+    // link to the point
     @IBAction func point(sender: UIButton)
     {
+        // only insert point if it is the first
         if numberIsFloat == false
         {
             display.text = display.text! + sender.currentTitle!
@@ -94,6 +108,7 @@ class ViewController: UIViewController
         }
     }
     
+    // link to C button. Sets calculator to original state
     @IBAction func clear()
     {
         display.text = "0"
@@ -101,26 +116,28 @@ class ViewController: UIViewController
         log.text = ""
     }
     
+    // array for holding operands
     var operandStack = Array<Double>()
     
+    // link to 'enter' button
     @IBAction func enter()
     {
+        // user is not typing and check for float is reset
         userIsTyping = false
         numberIsFloat = false
+        // add number on screen to stack
         operandStack.append(displayValue)
         
-        
-        
+        // if number is not yet added to log, add it
         if numberTyped != ""
         {
-            log.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+            log.lineBreakMode = NSLineBreakMode.ByTruncatingHead
             log.text = log.text! + numberTyped + ", "
             numberTyped = ""
         }
-        
-
     }
     
+    // variable to easily change and retrieve the value on screen as a Double
     var displayValue: Double
     {
         get
@@ -133,6 +150,5 @@ class ViewController: UIViewController
             userIsTyping = false
         }
     }
-    
 }
 
